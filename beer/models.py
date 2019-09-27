f699f17b8dfa58457939d273bd34836c92536f1a
Austin Carnahan <ac6404@gmail.com>
2019-09-27 18:40:31 -0500

    add models

--- beer/models.py
+++ beer/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    address1 = models.CharField(max_length=80, blank=True)
+    address2 = models.CharField(max_length=80, blank=True)
+    address3 = models.CharField(max_length=80, blank=True)
+    city = models.CharField(max_length=80, blank=False)
+    state = models.CharField(max_length=80, blank=True)
+    zipcode = models.IntegerField(blank=True)
+    country = models.CharField(max_length=80, blank=False)
+    lat = models.FloatField(blank=True)
+    lon = models.FloatField(blank=True)
+
+    def __str__(self):
+        return self.city + ", " + self.country
+
+class Brewery(models.Model):
+    name = models.CharField(max_length=80, blank=False)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Style(models.Model):
+    basic = models.CharField(max_length=60, blank=False)
+    detailed = models.CharField(max_length=80, blank=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Beer(models.Model):
+    name = models.CharField(max_length=80, blank=False)
+    abv = models.FloatField(blank=True)
+    ibu = models.IntegerField(blank=True)
+    description = models.CharField(max_length=200, blank=False)
+    brewery = models.ForeignKey(Brewery, on_delete=models.CASCADE)
+    style = models.ForeignKey(Style, on_delete=models.CASCADE)
+    sighting = models.ManyToManyField(Location, related_name='sightings')
+    tags = models.ManyToManyField(Tag, related_name='tags')
+
+    def __str__(self):
+        return self.name
+
+class Food(models.Model):
+    name = models.CharField(max_length=80, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Pairing(models.Model):
+    beer = models.ForeignKey(Beer, on_delete=models.CASCADE) 
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.food + " with " + self.beer
+
