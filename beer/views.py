532a98949b0dd65a390071f56684571f7c97712b
Austin Carnahan <ac6404@gmail.com>
2019-09-30 21:13:13 -0500

    add first endpoint

--- beer/views.py
+++ beer/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Beer
 from .serializers import BeerSerializer
+
+
+class ListBeersView(generics.ListAPIView):
+    """
+    Provides a GET method handler
+    """
+
+    queryset = Beer.objects.all()
+    serializer_class = BeerSerializer
